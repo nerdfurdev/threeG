ce8b16310deacfaeed6bec8a5f93b00b67b05ec9
furnickdev <furnickpersona@gmail.com>
2024-10-26 23:29:47 +0200

    init

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { ThreeMFLoader } from "three/examples/jsm/Addons.js";
 import Stats from "three/addons/libs/stats.module.js";
 import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 
